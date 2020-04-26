098c640153aab58545839b7eedab43949f1ee476
yogendra-yatnalkar <yogenyat@gmail.com>
2020-04-26 22:25:16 +0530

    Version 1.0.0

---
+++ src/app.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+# Flask
+from flask import Flask, redirect, url_for, request, render_template, Response, jsonify, redirect
+from util import *
+
+# Declare a flask app
+app = Flask(__name__)
+
+print('App loaded.')
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+
+def predict():
+    if request.method == 'POST':
+        # Get the image from post request
+        img = base64_to_pil(request.json)
+        ans_mode = "Flask is more important than model!! Dang!"
+        return jsonify(result=str(ans_mode))
+    return None
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="66", threaded=False)
